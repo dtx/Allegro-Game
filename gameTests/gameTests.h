9b44cd719449c836f9ad83e4be8a82dad1630bf3
Darshan Sanghani <d.gamer4@gmail.com>
2011-07-01 00:08:54 -0400

    first commit

---
+++ gameTests/gameTests.h
@@ -0,0 +1,17 @@
+//
+//  gameTests.h
+//  gameTests
+//
+//  Created by Darshan Sanghani on 6/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface gameTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ gameTests/gameTests.m
@@ -0,0 +1,33 @@
+//
+//  gameTests.m
+//  gameTests
+//
+//  Created by Darshan Sanghani on 6/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "gameTests.h"
+
+
+@implementation gameTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in gameTests");
+}
+
+@end
